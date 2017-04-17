6c9ef472f79a860ce488ce2a2d5e442c9bcc5cc3
ChrisMurphy <monkeeman69@googlemail.com>
2017-04-17 23:09:53 +0100

    feat(auth): social tests

--- src/app/auth/social/social.component.spec.ts
+++ src/app/auth/social/social.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { SocialComponent } from './social.component';
+import { AuthProviders } from 'angularfire2';
 
 describe('SocialComponent', () => {
   let component: SocialComponent;
@@ -9,10 +10,10 @@ describe('SocialComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SocialComponent ],
+      declarations: [SocialComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,7 +22,55 @@ describe('SocialComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create a component instance', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit a google auth event', () => {
+    // Spy on the authEvent emitter
+    spyOn(component.authEvent, 'emit');
+
+    // Click the google social auth button
+    fixture.nativeElement.querySelectorAll('#googleSocial')[0].click();
+
+    // Now we can check authEvent has been called with correct payload
+    expect(component.authEvent.emit).toHaveBeenCalledTimes(1);
+    expect(component.authEvent.emit).toHaveBeenCalledWith(AuthProviders.Google);
+  });
+
+  it('should emit a facebook auth event', () => {
+    // Spy on the authEvent emitter
+    spyOn(component.authEvent, 'emit');
+
+    // Click the facebook social auth button
+    fixture.nativeElement.querySelectorAll('#facebookSocial')[0].click();
+
+    // Now we can check authEvent has been called with correct payload
+    expect(component.authEvent.emit).toHaveBeenCalledTimes(1);
+    expect(component.authEvent.emit).toHaveBeenCalledWith(AuthProviders.Facebook);
+  });
+
+  it('should emit a twitter auth event', () => {
+    // Spy on the authEvent emitter
+    spyOn(component.authEvent, 'emit');
+
+    // Click the twitter social auth button
+    fixture.nativeElement.querySelectorAll('#twitterSocial')[0].click();
+
+    // Now we can check authEvent has been called with correct payload
+    expect(component.authEvent.emit).toHaveBeenCalledTimes(1);
+    expect(component.authEvent.emit).toHaveBeenCalledWith(AuthProviders.Twitter);
+  });
+
+  it('should emit a github auth event', () => {
+    // Spy on the authEvent emitter
+    spyOn(component.authEvent, 'emit');
+
+    // Click the github social auth button
+    fixture.nativeElement.querySelectorAll('#githubSocial')[0].click();
+
+    // Now we can check authEvent has been called with correct payload
+    expect(component.authEvent.emit).toHaveBeenCalledTimes(1);
+    expect(component.authEvent.emit).toHaveBeenCalledWith(AuthProviders.Github);
   });
 });
